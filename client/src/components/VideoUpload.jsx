2c29f17f5ad6f65a72c468257e39e4ab2424d5a1
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2023-08-28 19:53:30 +0530

    ✨ Ui Better Engagement

--- client/src/components/VideoUpload.jsx
+++ client/src/components/VideoUpload.jsx
@@ -1,9 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './VideoUpload.scss'
+import { useDropzone } from 'react-dropzone';
 
+function ProgressBar({ progress, estimatedRemainingTime }) {
+  const progressBarWidth = `${progress}%`;
+  const estimatedTimeText = `Estimated remaining time: ${estimatedRemainingTime}`;
+
+  return (
+    <div className="progress-container">
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div className="progress-out">
+          <div className="progress-bar" style={{ width: progressBarWidth }}></div>
+        </div>
+        <p style={{ display: 'flex', alignItems: 'center', width: '20%', justifyContent: 'center' }}>{progress.toFixed(2)}%</p>
+      </div>
+
+      <div className="estimated-time">
+        {estimatedTimeText}
+      </div>
+    </div>
+  );
+}
 function VideoUpload() {
   const [result, setResult] = useState(null);
   const [selectedFile, setSelectedFile] = useState(null);
   const [isLoading, setIsLoading] = useState(false); // New state for loading
+  const [progressData, setProgressData] = useState({
+    progress: 0,
+    estimated_remaining_time: ''
+  });
+
+  const handleRefresh = () => {
+    handleStop();
+  };
+
+  useEffect(() => {
+    const socket = io('http://127.0.0.1:5000');
+
+    socket.on('progress_update', (data) => {
+      setProgressData(data);
+    });
+
+    window.addEventListener('beforeunload', handleRefresh);
+
+    return () => {
+      socket.disconnect();
+      window.removeEventListener('beforeunload', handleRefresh);
+    };
+  }, []);  // Empty dependency array ensures the effect runs only once
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -11,7 +58,16 @@ function VideoUpload() {
 
   const handleAnalyze = async () => {
     if (selectedFile === null) {
-      window.alert("Select a file");
+      toast.error('Please Select a file', {
+        position: "top-center",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        progress: undefined,
+        theme: "dark",
+      });
       return;
     }
 
@@ -27,7 +83,6 @@ function VideoUpload() {
       });
 
       const data = await response.json();
-      console.log(data);
       setResult(data);
     } catch (error) {
       console.error('Error:', error);
@@ -36,17 +91,97 @@ function VideoUpload() {
     }
   };
 
+  const handleStop = async () => {
+    toast.warning('Process has been Interrupted', {
+      position: "top-center",
+      autoClose: 4000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      progress: undefined,
+      theme: "dark",
+    });
+    try {
+      const response = await fetch('http://127.0.0.1:5000/stop');
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  const onDrop = (acceptedFiles) => {  
+    if (!acceptedFiles[0].type.startsWith("video/")) {
+      toast.error('Please Upload Video File', {
+        position: "top-center",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        progress: undefined,
+        theme: "dark",
+      });
+      return;
+    }
+    setSelectedFile(acceptedFiles[0]);
+  };
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: 'mp4',
+    multiple: false,
+  });
+
   return (
     <div className="App">
-      <h1>Person Detection App</h1>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <input type="file" accept="video/*" onChange={handleFileChange} />
-        <button onClick={handleAnalyze} disabled={isLoading}>
-          {isLoading ? 'Analyzing...' : 'Analyze Video'}
-        </button>
+      <h1 style={{ textAlign: 'center', color: '#fff' }}>Video Analyzer</h1>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+        <div {...getRootProps()} className="drop-zone">
+          <input accept="mp4" {...getInputProps()} />
+          {selectedFile ? (
+            <p>Selected File: {selectedFile.name}</p>
+          ) : (
+            <>
+              <p><i className="fa-solid fa-cloud-arrow-up"></i></p>
+              <p>Drag and drop a video file here, or click to select a file.</p>
+            </>
+          )}
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '30px' }}>
+          <button onClick={handleAnalyze} disabled={isLoading}>
+            {isLoading ? 'Analyzing...' : 'Analyze Video'}
+          </button>
+          {
+
+            isLoading ?
+              (
+                <button className='stop' onClick={handleStop} disabled={!isLoading}>
+                  Stop
+                </button>
+              )
+              :
+              (
+                null
+              )
+          }
+        </div>
       </div>
       {isLoading ? (
-        <p>Loading...</p>
+        <>
+          <div class="panel">
+            <div class="scanner"></div>
+            <ul class="something">
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+
+          <ProgressBar progress={progressData.progress} estimatedRemainingTime={progressData.estimated_remaining_time} />
+        </>
       ) : (
         result && (
           <div>
@@ -56,7 +191,7 @@ function VideoUpload() {
                 <ul>
                   {result.map((frame, index) => (
                     <li key={index}>
-                      Frame {index + 1}: {frame[0].class} found at timestamp {frame[0].timestamp}sec.
+                      Frame {index + 1}: {frame[0].class} detected at timestamp {frame[0].timestamp.toFixed(2)} sec.
                     </li>
                   ))}
                 </ul>
@@ -67,6 +202,19 @@ function VideoUpload() {
           </div>
         )
       )}
+
+      <ToastContainer
+        position="top-center"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover={false}
+        theme="dark"
+      />
     </div>
   );
 }
--- server/server.py
+++ server/server.py
@@ -2,14 +2,25 @@ from ultralytics import YOLO
 from pathlib import Path
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_socketio import SocketIO, emit
 import cv2
 import time
 import math
 import json
 
-
 app = Flask(__name__)
 CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")  # Adjust origins for production
+
+@socketio.on('connect')
+def handle_connect():
+    emit('status', {'message': 'Analysis started'})
+
+def send_progress(progress):
+    socketio.emit('progress', {'message': progress})
+
+def send_results(results):
+    socketio.emit('results', {'data': results})
 
 # Define class names
 class_names = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
@@ -22,8 +33,20 @@ class_names = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'tra
              'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
              'teddy bear', 'hair drier', 'toothbrush' ]
 
+stop_processing = False  # Flag to indicate whether processing should stop
+
+@app.route("/stop", methods=['GET'])
+def stop_analysis():
+    global stop_processing 
+    stop_processing = True
+    return ({"message": "Processing stopped"}), 400
+
 @app.route("/analyze", methods=['POST', 'GET'])
 def analysis():
+    global stop_processing 
+    if(stop_processing == True):
+        stop_processing = False
+        
     video_file = request.files['video']
     if not video_file:
         return ({'error': 'No video file provided'}), 400
@@ -40,9 +63,17 @@ def analysis():
     detection_results = []
     initial_timestamp = 0
     
-    while cap.isOpened():
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # Total number of frames in the video
+    frames_per_second = cap.get(cv2.CAP_PROP_FPS)
+    current_frame = 0
+    processing_time_sum = 0
+    processed_frames = 0
+    
+    while cap.isOpened() and not stop_processing:
         ret, frame = cap.read()
         if not ret:
+            break
+        if stop_processing:
             break
         
         start_time = time.time()
@@ -71,7 +102,7 @@ def analysis():
                         initial_timestamp = current_timestamp
                     else :    
                         delta_time = current_timestamp - initial_timestamp
-                        if(delta_time > 1):
+                        if(delta_time > 3):
                             detections.append({
                                 "class": currentClass,
                                 "confidence": confidence,
@@ -79,13 +110,47 @@ def analysis():
                             })
                             detection_results.append(detections)
                             initial_timestamp = current_timestamp
+                            
+        end_time = time.time()
+        # Processing current frame
+        current_frame += 1
+        # Calculate percentage progress completed 
+        progress = calculate_progress(current_frame, frame_count)
+        # Estimate remaining time
+        
+        processing_time = end_time - start_time
+        processing_time_sum += processing_time
+        remaining_frames = frame_count - current_frame
+        
+                # Calculate average processing time per frame
+        avg_processing_time = processing_time_sum / current_frame
+        
+        estimated_remaining_time = calculate_remaining_time(remaining_frames,  avg_processing_time)
+        
+        # Backend logs
+        log_data = {
+            'progress': progress,
+            'estimated_remaining_time': estimated_remaining_time
+            # Add more relevant information here
+        }
+        socketio.emit('progress_update', log_data)
 
     cap.release()
     cv2.destroyAllWindows()
     
     return (detection_results)
 
+def calculate_progress(current_frame, total_frames):
+    return (current_frame / total_frames) * 100
 
+def calculate_remaining_time(remaining_frames, avg_processing_time):
+    if avg_processing_time == 0:
+        return "N/A"  # Avoid division by zero
+
+    seconds_remaining = remaining_frames * avg_processing_time
+    minutes = int(seconds_remaining // 60)
+    seconds = int(seconds_remaining % 60)
+    return f"{minutes} min {seconds} sec"
 
 if __name__ == "__main__":
     app.run(debug=True)
